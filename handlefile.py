e012ec47d6ce5cb5354750fd75bf249cadd7205e
Tatsuhiko Ikemoto <qt4cpp@gmail.com>
2023-10-19 06:34:01 +0900

    optimized header

--- handlefile.py
+++ handlefile.py
@@ -1,6 +1,5 @@
 import csv
 
-import numpy as np
 import pandas as pd
 
 
--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import pandas as pd
-
 import handlefile
-from manage_examlist import get_unchecked_index, get_departments_from_df, get_unchecked_list
+from manage_examlist import get_departments_from_df, get_unchecked_list
 
 if __name__ == '__main__':
     unchecked_list = get_unchecked_list(sort=True)
