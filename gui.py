11d6161c0e036a627d6c66131a7756ba4838fe08
Tatsuhiko Ikemoto <qt4cpp@gmail.com>
2023-10-24 11:01:10 +0900

    migrate using class to make ui.

--- gui.py
+++ gui.py
@@ -2,25 +2,39 @@ import tkinter as tk
 import tkinter.filedialog
 
 
-def filedialog_open():
-    file_path = tk.filedialog.askopenfilename(
-        title='csvファイルを指定してください',
-        filetypes=[("CSV ファイル", ".csv")],
-        initialdir='./'
-    )
-    entry.delete(0, tk.END)
-    entry.insert(0, file_path)
+class UncheckedList(tk.Frame):
+
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.label = None
+        self.entry = None
+        self.button = None
+        self.pack()
+        self.set_ui()
+
+    def set_ui(self):
+        self.label = tk.Label(self, text="csvファイルを指定してください。")
+        self.label.pack()
+
+        self.entry = tk.Entry(self)
+        self.entry.pack(side=tk.LEFT)
+
+        self.button = tk.Button(self, text="Browse", command=self.filedialog_open)
+        self.button.pack(side=tk.RIGHT)
+
+    def filedialog_open(self):
+        file_path = tk.filedialog.askopenfilename(
+            title='csvファイルを指定してください',
+            filetypes=[("CSV ファイル", ".csv")],
+            initialdir='./'
+        )
+        self.entry.delete(0, tk.END)
+        self.entry.insert(0, file_path)
 
 
 root = tk.Tk()
-label = tk.Label(root, text="csvファイルを指定してください。")
-label.pack()
-entry = tk.Entry(root)
-entry.pack(side=tk.LEFT)
 
-button = tk.Button(root, text="Browse", command=filedialog_open)
-button.pack(side=tk.RIGHT)
-
+app = UncheckedList(master=root)
 root.title(u"未読影リスト")
 root.geometry("320x120")
 root.mainloop()
