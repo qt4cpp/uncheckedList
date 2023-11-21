5be59d446d75737720715b9a477f0c69e90045f9
Tatsuhiko Ikemoto <qt4cpp@gmail.com>
2023-11-21 21:39:25 +0900

    ダイアログで指定したファイルを開くようにした。

--- gui.py
+++ gui.py
@@ -2,31 +2,38 @@ import os.path
 import tkinter as tk
 import tkinter.filedialog
 
+import handlefile
+from handleexcel import set_styles
+from manage_examlist import get_unchecked_table, get_departments_from_df
+
 
 class UncheckedList(tk.Frame):
 
-    def __init__(self, master=None):
+    def __init__(self, header_path, master=None):
         super().__init__(master)
         self.file_path = ""
+        self.header_path = header_path
         self.frame = tk.Frame(master)
         self.frame.pack()
         self.label_frame = None
         self.csv_entry = None
         self.browse_button = None
+        self.start_button = None
         self.pack()
         self.set_ui()
 
     def set_ui(self):
         self.label_frame = tk.LabelFrame(self.frame, text='csvファイル')
         self.label_frame.grid(row=0, column=0, sticky="news", padx=10, pady=10)
-        # self.label = tk.Label(self, text="csvファイルを指定してください。")
-        # self.label.pack()
 
         self.csv_entry = tk.Entry(self.label_frame)
         self.csv_entry.grid(row=0, column=0)
 
         self.browse_button = tk.Button(self.label_frame, text="Browse", command=self.filedialog_open)
         self.browse_button.grid(row=0, column=1)
+
+        self.start_button = tk.Button(self, text="Start", command=self.output_unchecked_list)
+        self.start_button.grid(row=1, column=1)
 
     def filedialog_open(self):
         file_path = tk.filedialog.askopenfilename(
@@ -42,9 +49,15 @@ class UncheckedList(tk.Frame):
     def csv_path(self):
         return self.csv_entry.get()
 
+    def output_unchecked_list(self):
+        unchecked_table = get_unchecked_table(self.csv_path, self.header_path, sort=True)
+        print('診療科取得')
+        departments = get_departments_from_df(unchecked_table)
+        print('診療科取得完了')
 
-root = tk.Tk()
-app = UncheckedList(master=root)
-root.title(u"未読影リスト")
-root.geometry("480x220")
-root.mainloop()
+        print('excelに保存する')
+        handlefile.write_excel('test.xlsx', unchecked_table, departments)
+        print('excelに保存完了')
+        wb = handlefile.open_excel('test.xlsx')
+        set_styles(wb)
+
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import handlefile
+import tkinter as tk
 from handleexcel import set_styles
 from manage_examlist import get_departments_from_df, get_unchecked_table
+from gui import UncheckedList
+
 
 if __name__ == '__main__':
-    csv_path = 'data/未確定1026.csv'
-    header_path = 'header'
-    unchecked_table = get_unchecked_table(csv_path=csv_path, header_path=header_path, sort=True)
-    departments = get_departments_from_df(unchecked_table)
-
-    handlefile.write_excel('test.xlsx', unchecked_table, departments)
-    wb = handlefile.open_excel('test.xlsx')
-    set_styles(wb)
+    root = tk.Tk()
+    app = UncheckedList('header', master=root)
+    root.title(u"所見未確認リスト")
+    root.geometry("480x240")
+    csv_path = app.csv_path
+    root.mainloop()
